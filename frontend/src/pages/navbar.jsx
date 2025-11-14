ceb3c828c06e66d9620a8aa5feeb27ca0a3f0f59
Serena Shen <83137666+ChaoyangS@users.noreply.github.com>
2025-11-14 15:47:52 -0500

    added link for nav

--- frontend/src/pages/navbar.jsx
+++ frontend/src/pages/navbar.jsx
@@ -1,12 +1,35 @@
+// import React from "react";
+// import "./navbar.css";
+
+// const NavBar = () => {
+//   return (
+//     <header className="landing-nav">
+//       <div className="nav-logo">Carly</div>
+//       <nav className="nav-links">
+//         <button className="nav-link">Find</button>
+//         <button className="nav-link">Contact</button>
+//         <button className="nav-link">Help</button>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default NavBar;
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./navbar.css";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="landing-nav">
       <div className="nav-logo">Carly</div>
       <nav className="nav-links">
-        <button className="nav-link">Find</button>
+        <button className="nav-link" onClick={() => navigate("/quiz")}>
+          Find
+        </button>
+
         <button className="nav-link">Contact</button>
         <button className="nav-link">Help</button>
       </nav>
